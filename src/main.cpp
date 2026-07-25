ecfdf40f176327a532b53d2fcb1044feb1be0efb
notlbock <uyenla1224@gmail.com>
2026-07-25 11:31:14 -0700

    Redo main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>
-#include <format>
 #include <cstdlib>
+#include <format>
 
 #define WINDOWX 800
 #define WINDOWY 600
@@ -10,22 +10,22 @@ namespace main {
     const char *title = "SDL2 Examples";
     SDL_Renderer *renderer = nullptr;
     SDL_Window *window = nullptr;
-};
+}
 
 void initSDL(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         auto error = std::format("Error initializing SDL2: {}", SDL_GetError());
         std::cerr << error << std::endl;
-        std::exit(1); // error
+        std::exit(1);
     }
 }
 
 void createObjects(void) {
     main::window = SDL_CreateWindow(
-        main::title,
+        title,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOWX, WINDOWY,
-        0
+        0       
     );
 
     if (!main::window) {
@@ -40,16 +40,16 @@ void createObjects(void) {
         SDL_RENDERER_ACCELERATED        
     );
 
-    if (!main::renderer) {
+    if (main::renderer) {
         auto error = std::format("Error creating renderer: {}", SDL_GetError());
         std::cerr << error << std::endl;
         std::exit(1);
     }
 }
 
-void close(void) {
+void closeSDL(void) {
     SDL_DestroyRenderer(main::renderer);
-    SDL_DestroyWindow(main::renderer);
+    SDL_DestroyWindow(main::window);
     SDL_Quit();
 }
 
@@ -57,7 +57,6 @@ int main(void) {
     initSDL();
     createObjects();
 
-    // mainloop
     int running = 1;
     SDL_Event event;
     while (running) {
@@ -67,10 +66,9 @@ int main(void) {
             }
         }
         SDL_SetRenderDrawColor(main::renderer, 255, 255, 255, 255);
-        SDL_RenderClear();
-        SDL_RenderPresent();
+        SDL_RenderClear(main::renderer);
+        SDL_RenderPresent(main::renderer);
     }
-
-    close();
+    closeSDL();
     return 0;
 }
